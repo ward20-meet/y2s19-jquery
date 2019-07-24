9177464bda85a07f4a817d6b07accf8c73e5efe3
myname21-meet <myname21@meet.mit.edu>
2019-07-24 09:42:14 -0400

    lab1

--- style/style.js
+++ style/style.js
@@ -3,16 +3,16 @@
 
 
 function updateSize(newSize) {
-  // FINISH ME! Set the width of the MEET logo to be newSize in pixels.
+$("img").eq(0).css("width",newSize);
 }
 
 
 function updateLeftPosition(newX) {
-	// FINISH ME
+$("img").eq(0).css("left",newX);
 
 }
 
 function updateTopPosition(newY) {
-	// FINISH ME
+$("img").eq(0).css("top",newY);
 }
 
